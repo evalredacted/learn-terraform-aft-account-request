f08bafcf8f358e6f03dc1b5718948e4d04db8b63
EvalRedacted <27139920+evalredacted@users.noreply.github.com>
2022-02-10 08:23:58 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,3 +54,31 @@ module "sandbox2" {
 
   account_customizations_name = "sandbox"
 }
+  
+module "dev-devops-eks" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "evalredacted+dev+ct+dev-devops-eks@gmail.com"
+    AccountName               = "dev-devops-eks"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "evalredacted+dev+ct@gmail.com"
+    SSOUserFirstName          = "Devops"
+    SSOUserLastName           = "EKS"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
